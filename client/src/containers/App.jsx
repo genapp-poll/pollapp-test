73c95ecb331a8352c67a03788a7ef78e72650785
Alen Niu <43788415+alenniu@users.noreply.github.com>
2022-02-05 16:37:18 -0800

    setup entire redux and connection with backedn

---
+++ client/src/containers/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import decode from "jwt-decode";
+
+import { store } from "../store/index";
+import { setCurrentUser, addError, setToken } from "../store/actions";
+
+if (localStorage.jwtToken) {
+  setToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(decode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+    store.dispatch(addError(err));
+  }
+}
+
+const App = () => (
+  <Provider store={store}>
+    <div>App works</div>
+  </Provider>
+);
+
+export default App;
